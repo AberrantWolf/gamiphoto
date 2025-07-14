1ecb9bf16ece6fc20308d46b00bd67e9591f4637
Scott Harper <orcein@gmil.com>
2025-07-15 08:15:38 +0900

    Trying out bevy_ui, so far just modifying some example code to try to understand it better

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use bevy::{color::palettes::css::*, prelude::*, winit::WinitSettings};
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (
+            &Interaction,
+            &mut BackgroundColor,
+            &mut BorderColor,
+            &Children,
+        ),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                **text = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = RED.into();
+            }
+            Interaction::Hovered => {
+                **text = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = WHITE.into();
+            }
+            Interaction::None => {
+                **text = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = BLACK.into();
+            }
+        }
+    }
+}
+
+fn _button(asset_server: &AssetServer) -> impl Bundle + use<> {
+    (
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Button,
+            Node {
+                width: Val::Px(150.0),
+                height: Val::Px(65.0),
+                border: UiRect::all(Val::Px(5.0)),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BorderColor(Color::BLACK),
+            BorderRadius::MAX,
+            BackgroundColor(NORMAL_BUTTON),
+            children![(
+                Text::new("Button"),
+                TextFont {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 33.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                TextShadow::default(),
+            )]
+        )],
+    )
+}
+
+fn main_ui(_asset_server: &AssetServer) -> impl Bundle + use<> {
+    let selection_layout = Node {
+        flex_direction: FlexDirection::Column,
+        justify_content: JustifyContent::Start,
+        align_items: AlignItems::Start,
+        padding: UiRect::all(Val::Px(16.0)),
+        ..default()
+    };
+
+    let top_layout = Node {
+        width: Val::Percent(100.0),
+        height: Val::Percent(100.0),
+        flex_direction: FlexDirection::Row,
+        justify_content: JustifyContent::Start,
+        align_items: AlignItems::Stretch,
+        ..default()
+    };
+
+    (
+        top_layout,
+        children![(
+            selection_layout,
+            BackgroundColor(NORMAL_BUTTON),
+            children![
+                Text::new("First"),
+                Text::new("Second")
+            ]
+        )]
+    )
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    // ui camera
+    commands.spawn(Camera2d);
+    commands.spawn(main_ui(&assets));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::desktop_app())
+        .add_systems(Startup, setup)
+        .add_systems(Update, button_system)
+        .run();
+}
